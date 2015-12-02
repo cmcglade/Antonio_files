ce7ba3b00123924bacc3b25aef0968988077fa80
AFernando <antonio_fdo@yahoo.ca>
2015-12-02 09:44:28 -0500

    adding my rest of files of practise in php

---
+++ basic.php
@@ -0,0 +1,43 @@
+<!Doctype html>
+
+<html>
+
+<head> 
+
+<title>yoooooooooooo</title>
+
+
+
+
+</head>
+
+<body>
+
+
+
+<h1>This is a php command</h1>
+
+<p>
+
+<?php echo "Hello this is the php echo command<br>"; ?>
+    phpinfo();>  
+    
+    <?php echo "hello" . "class"; ?> 
+    
+    $var_a = 1; 
+    echo $var_a;
+    $var_b = 2;
+    echo $var_b; 
+    $var_c =  $var_a + $var_b
+    
+    echo $var_c; 
+    
+    $houseSpaces = "Hello"; 
+    
+    ?> 
+    
+    </p> 
+
+
+</body>
+</html>
---
+++ inclass.php
@@ -0,0 +1,30 @@
+<!Doctype html>
+
+<html>
+
+<head> 
+<?php>
+    $var = "PHP Tutorial" ?> 
+
+<title><?php echo $var; ?></title>
+
+
+
+
+</head>
+
+<body>
+
+<h1><?php echo $var; ?></h1>h1
+
+<p> Some text php </p>
+
+<p> for more info <href=http://php.net">
+
+</body> 
+    
+
+
+
+
+
